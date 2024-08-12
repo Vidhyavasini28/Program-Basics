b1856e0a04e1d46734c8714904d67e6334aa85ba
Vidhyavasini28 <psvidhyavasini@gmail.com>
2024-08-12 19:01:58 +0530

    count even numbers

---
+++ count_no_of_even_numbers_in_the_array.js/count_even_number.js
@@ -0,0 +1,9 @@
+arr=[12,13,451,56,31,76,89,34]
+
+let count=0
+for(let i=0;i<arr.length;i++){
+    if(arr[i]%2===0){
+        count=count+1
+    }
+}
+console.log("The total count of even numbers in the array:",count)
