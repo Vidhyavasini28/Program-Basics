a35a5fd5233bdbe3bb03ad779c0ee6b3be0773bb
Vidhyavasini28 <psvidhyavasini@gmail.com>
2024-08-14 21:33:24 +0530

    index first occurence of specific element

---
+++ index_of_first_occurence_of_element_in_array.js/index_first_occurence.js
@@ -0,0 +1,10 @@
+let arr=[45,87,6,23,89,67,3,45,90,84,54]
+
+let num=23
+
+for(var i=0;i<arr.length;i++){
+    if(arr[i]==num){
+        break;
+    }
+}
+console.log("Index of first occurence of element in the array:",i)
