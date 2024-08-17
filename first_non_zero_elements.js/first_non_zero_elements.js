6cca06e0a6a46c8ed7d75e7243d2903cbb5dc54b
Vidhyavasini28 <psvidhyavasini@gmail.com>
2024-08-17 18:40:28 +0530

    First_non_zero_elements

---
+++ first_non_zero_elements.js/first_non_zero_elements.js
@@ -0,0 +1,8 @@
+let arr=[35,75,3,12,63,14]
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]!=0){
+        console.log("the first non zero elements in the array is ",arr[i])
+        break;
+    }
+}      
